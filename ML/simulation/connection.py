4aeceae1082059f6e9b10b73d50a13f3c272ea3a
Etai zilberman <etai444@gmail.com>
2024-12-30 18:01:15 +0200

    added simulation content merged main to PPOimpl for running carla with model

---
+++ ML/simulation/connection.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import carla
+from settings import PORT, TIMEOUT, HOST
+
+class ClientConnection:
+    def __init__(self, town):
+        self.client = None
+        self.town = town
+
+    def setup(self):
+        try:
+            # Connecting to the  Server
+            self.client = carla.Client(HOST, PORT)
+            self.client.set_timeout(TIMEOUT)
+            self.world = self.client.load_world(self.town)
+            self.world.set_weather(carla.WeatherParameters.CloudyNoon)
+            return self.client, self.world
+
+        except Exception as e:
+            print(
+                'Failed to make a connection with the server: {}'.format(e))
+            self.error()
+
+    # An error method: prints out the details if the client failed to make a connection
+    def error(self):
+
+        print("\nClient version: {}".format(
+            self.client.get_client_version()))
+        print("Server version: {}\n".format(
+            self.client.get_server_version()))
+
+        if self.client.get_client_version != self.client.get_server_version:
+            print(
+                "There is a Client and Server version mismatch! Please install or download the right versions.")
