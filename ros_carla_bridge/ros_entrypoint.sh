7aaa238d050ec6500945f5502ff19b7766202f5d
unknown <barara98@gmail.com>
2025-01-04 16:08:00 +0200

    carla_ros_bridge dockerfile

--- ros_carla_bridge/ros_entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-source /opt/ros/foxy/setup.bash
-source /root/carla-ros-bridge/install/setup.bash
-
-# exec "$@"
-roslaunch carla_ros_bridge carla_ros_bridge.launch host:=192.168.1.50 port:=2000
