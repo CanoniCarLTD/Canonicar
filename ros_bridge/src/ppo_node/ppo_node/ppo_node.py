ec2c52db95d97b7094dea6cdff163ea178ba3504
Etaizil <etai444@gmail.com>
2025-01-27 17:36:04 +0200

    Changed to a single topic subscription for data arrival

--- ros_bridge/src/ppo_node/ppo_node/ppo_node.py
+++ ros_bridge/src/ppo_node/ppo_node/ppo_node.py
@@ -4,55 +4,27 @@ from rclpy.node import Node
 from std_msgs.msg import Float32MultiArray
 import torch
 import numpy as np
-from client_node import spawn_vehicle
-
 
 class PPOModelNode(Node):
     def __init__(self):
         super().__init__("ppo_model_node")
-        """
-        These are not the right topics.
-        Need to subscribe to the right topics after processing the data
-        """
         
-        # Need a topic to subscribe to convolution
-        self.image_sub = self.create_subscription(
-            Image, "/carla/rgb_front/image_raw", self.image_callback, 10
-        )
-        self.lidar_sub = self.create_subscription(
-            PointCloud2, "/carla/lidar/points", self.lidar_callback, 10
-        )
-        self.imu_sub = self.create_subscription(
-            Imu, "/carla/imu/imu", self.imu_callback, 10
-        )
-        self.gnss_sub = self.create_subscription(
-            NavSatFix, "/carla/gnss/gnss", self.gnss_callback, 10
+        # CHANGE THE TOPIC PATH
+        self.data_sub = self.create_subscription(
+            Float32MultiArray, "/carla/###/###", self.data_callback, 10
         )
 
         # Action publisher (Steering, Throttle, Brake)
-        # Publish the action data in another node?
+        # CHANGE THE TOPIC PATH
         self.action_publisher = self.create_publisher(
             Float32MultiArray, "/carla/vehicle_control", 10
         )
         
-        self.get_logger().info("PPOModelNode initialized and subscribed to topics.")
+        self.get_logger().info("PPOModelNode initialized and subscribed to data topic.")
 
-    def image_callback(self, msg):
+    def data_callback(self, msg):
         self.get_logger().info(
             f"Received image with resolution {msg.width}x{msg.height}"
-        )
-
-    def lidar_callback(self, msg):
-        self.get_logger().info(f"Received LiDAR data with {msg.width} points")
-
-    def imu_callback(self, msg):
-        self.get_logger().info(
-            f"Received IMU data: orientation [{msg.orientation.x}, {msg.orientation.y}, {msg.orientation.z}]"
-        )
-
-    def gnss_callback(self, msg):
-        self.get_logger().info(
-            f"Received GNSS data: Latitude {msg.latitude}, Longitude {msg.longitude}"
         )
 
 
