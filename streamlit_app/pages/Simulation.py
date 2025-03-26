33f5c6b1acf886968899de703c02b7d083074de7
unknown <barara98@gmail.com>
2025-03-26 12:26:48 +0200

    Embed canonicar live video in the simulation page

---
+++ streamlit_app/pages/Simulation.py
@@ -0,0 +1,35 @@
+import streamlit as st
+
+# Custom CSS for full-screen video and centered title
+st.markdown("""
+    <style>
+        .title {
+            text-align: center;
+            font-size: 40px;
+            font-weight: bold;
+        }
+        .video-container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 80vh; 
+        }
+        iframe {
+            width: 90vw; 
+            height: 80vh; 
+            border-radius: 10px; 
+        }
+    </style>
+""", unsafe_allow_html=True)
+
+# Centered title
+st.markdown('<div class="title">Live YouTube Video</div>', unsafe_allow_html=True)
+
+video_id = "yUa9mgTmDj8" 
+live_embed = f"""
+<div class="video-container">
+    <iframe src="https://www.youtube.com/embed/{video_id}?autoplay=1&controls=0&modestbranding=1&rel=0&vq=hd1080" 
+    frameborder="0" allow="autoplay; encrypted-media; fullscreen" allowfullscreen></iframe>
+</div>
+"""
+st.markdown(live_embed, unsafe_allow_html=True)
