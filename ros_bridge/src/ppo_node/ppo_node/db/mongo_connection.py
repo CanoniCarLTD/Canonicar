47d6c0c308ceb6c405cc9c09557a1930d99be0b3
unknown <barara98@gmail.com>
2025-03-26 22:32:58 +0200

    ADd envirmonemnt of the mongobd in the docker compose file

--- ros_bridge/src/ppo_node/ppo_node/db/mongo_connection.py
+++ ros_bridge/src/ppo_node/ppo_node/db/mongo_connection.py
@@ -2,16 +2,12 @@
 from mongoengine import connect, disconnect
 from pymongo import MongoClient
 import os
-from dotenv import load_dotenv
 
 def init_db():
-    load_dotenv(dotenv_path="/ros_bridge/src/.env")
     try:
         conn_str = os.getenv("MONGO_CONNECTION_STRING")
-        print(f"print the conn str: {conn_str}")
         client = MongoClient(conn_str)
         db_name = os.getenv("MONGO_DB")
-        print(f"db name: {conn_str}")        
         db = client[db_name]
         print("✅ Successfully connected to MongoDB Atlas")
         return db
