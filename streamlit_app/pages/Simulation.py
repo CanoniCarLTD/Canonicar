9c4576179ba13d43ae0bb3b6c3313d46b003b2c5
unknown <barara98@gmail.com>
2025-03-29 17:26:16 +0300

    Updated overview page for streamlit

--- streamlit_app/pages/Simulation.py
+++ streamlit_app/pages/Simulation.py
@@ -23,9 +23,9 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # Centered title
-st.markdown('<div class="title">Live YouTube Video</div>', unsafe_allow_html=True)
+st.markdown('<div class="title">Canonicar Live Simulation</div>', unsafe_allow_html=True)
 
-video_id = "DFOeRcof5Zs" 
+video_id = "DFOeRcof5Zs"
 live_embed = f"""
 <div class="video-container">
     <iframe src="https://www.youtube.com/embed/{video_id}?autoplay=1&controls=0&modestbranding=1&rel=0&vq=hd1080" 
