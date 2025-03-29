3dbdd5d116003f47a9c30da019eedab13d998429
unknown <barara98@gmail.com>
2025-03-29 20:28:47 +0300

    New tensorboard url

--- streamlit_app/pages/Tensor_board.py
+++ streamlit_app/pages/Tensor_board.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 # Replace with your actual ngrok URL
 tensorboard_url = "https://0201-5-29-228-0.ngrok-free.app"  # ← change this!
+print(tensorboard_url)
 
 st.title("📊 Canonicar ML Dashboard")
 st.subheader("Live TensorBoard Logs")
